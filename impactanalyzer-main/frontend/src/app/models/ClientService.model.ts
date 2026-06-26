01057531d3e7c38d539a4f199f4249456ab80c88
Fatiha Belhaj <fatihabelhaj143@gmail.com>
2026-06-26 12:11:04 +0100

    update frontend structure and UI theme

--- impactanalyzer-main/frontend/src/app/models/ClientService.model.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from './client.model';
-import { Service } from './service.model';
-
-export interface ClientService {
-  id: number;
-  client : Client;
-  service: Service;
-}
--- impactanalyzer-main/frontend/src/app/models/dependency.model.ts
+++
@@ -1,9 +0,0 @@
-import { Service } from './service.model';
-
-export interface Dependency {
-  id: number;
-  service: Service;
-  dependsOn: Service;
-  criticality: string;
-  dependencyType: string;
-}
